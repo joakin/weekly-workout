4f6345e767f9a880b9d79a95e62b1e7772c025fb
joakin <joaquin@chimeces.com>
2025-03-13 13:02:54 +0100

    Extract exercise list component

---
+++ data/exercise.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @typedef {Object} Exercise
+ * @property {string} name
+ * @property {import("./number-range.js").NumberRange} sets
+ * @property {import("./number-range.js").NumberRange} reps
+ * @property {string} [notes]
+ * @property {Exercise} [superset]
+ */
+
+export {};
